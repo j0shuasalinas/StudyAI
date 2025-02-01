70a5936c6b8c6fedf605578b009f07a00b9c1e15
dryhour <derekp9210@gmail.com>
2025-01-31 22:47:40 -0500

    feat: added loading screen

--- StudyAI/main.js
+++ StudyAI/main.js
@@ -1,3 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+import { IntroductionRandomText, LoadingText } from './utils/texts';
+import { LoadingMessageTime, OutAnimation } from './utils/animation';
+
 function Onboarding(){
     console.log("onboarding")
 }
